b3bed2198bac6c070ec88a7f6fb4610d6ebb55c5
Prachi Lakha <prachilakha@gmail.com>
2026-05-16 01:08:50 +0530

    Day 3: Auth backend complete - signup, login, JWT, middleware

--- railsmart/server/routes/authRoutes.js
+++ railsmart/server/routes/authRoutes.js
@@ -1,12 +1,15 @@
- const express = require('express')
+const express = require('express')
 const router = express.Router()
+const { signup, login } = require('../controllers/authController')
+const protect = require('../middleware/authMiddleware')
 
-router.post('/signup', (req, res) => {
-  res.json({ message: 'Signup route working' })
-})
+// Public routes
+router.post('/signup', signup)
+router.post('/login', login)
 
-router.post('/login', (req, res) => {
-  res.json({ message: 'Login route working' })
+// Protected test route
+router.get('/profile', protect, (req, res) => {
+  res.json({ message: 'Protected route works!', user: req.user })
 })
 
 module.exports = router
